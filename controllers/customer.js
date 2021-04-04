fc5daeb5ec961e2d0a14c60e4260970536f387e7
ahmed eid <ahmedeid6842@gmail.com>
2021-04-04 06:41:23 +0200

    add customer controllers for CRUD

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,12 +1,24 @@
-const mongoose = require("mongoose");
+const { ObjectId } = require("mongodb")
 const _ = require("lodash");
 
+const _db = require("../helper/db").getDB
+
 module.exports.getCustomers = async (req, res) => {
-    if (customer.length === 0) return res.status(404).send("no customers found")
-    return res.status(200).send(customer);
+    const customers = await _db()
+        .db()
+        .collection("customers")
+        .find()
+        .toArray();
+    if (customers.length === 0) return res.status(404).send({ msg: "no customers found" })
+
+    return res.status(200).send(customers);
 }
 
 module.exports.getCustomer = async (req, res) => {
+    const customer = await _db()
+        .db()
+        .collection("customers")
+        .findOne({ _id: ObjectId(req.params.id) });
 
     if (!customer) return res.status(404).send("no customer found")
 
@@ -14,16 +26,20 @@ module.exports.getCustomer = async (req, res) => {
 }
 
 module.exports.addCutomer = async (req, res) => {
-
-    const { error } = validateCustomer(req.body)
-    if (error) return res.status(402).send(error.message);
+    // validate a customer
 
     try {
+        const user = await _db()
+            .db()
+            .collection("customers")
+            .insertOne(req.body);
+
         return res.status(201).send({
-            _id: _.pick(user, ["_id"]),
+            _id:_.pick(user, ["insertedId"]),
             msg: "added successfully"
         })
     } catch (err) {
+        res.send(err)
         //email is unique
     }
 }
@@ -32,6 +48,15 @@ module.exports.updateCutomer = async (req, res) => {
 
     try {
         //update a customer
+        const { value: customer } = await _db() //destrucuture the returned object "value" rename "customer"
+            .db()
+            .collection("customers")
+            .findOneAndUpdate( //use "findOneAndUpdate" instead of "updateOne" because it can return the updated docuement ,,, same thing in delete
+                { _id: ObjectId(req.params.id) },
+                { $set: req.body },
+                { returnOriginal: false } //to return the deleted document
+            )
+
         if (!customer) return res.status(404).send("no customer with that ID");
 
         return res.status(201).send({
@@ -41,13 +66,23 @@ module.exports.updateCutomer = async (req, res) => {
     } catch (err) {
         return res.send(err.message); //validate id - must be 12 character 
     }
-    
+
 }
 
 module.exports.deleteCutomer = async (req, res) => {
 
     try {
-        if (!customer) return res.status(404).send("no customer with that ID");
+        //delete acusotmer
+        const { value: customer } = await _db()
+            .db()
+            .collection("customers")
+            .findOneAndDelete(
+                { _id: ObjectId(req.params.id) },
+                { returnOriginal: true } //to return the deleted document
+            )
+
+        if (!customer) return res.status(404).send({ msg: "no customer with that ID" });
+
         return res.status(200).send({
             customer,
             msg: "deleted successfully"
--- routes/customer.js
+++ routes/customer.js
@@ -7,9 +7,9 @@ const { isAdmin } = require("../middleware/isAdmin");
 
 router.get("/", customerControllers.getCustomers)
 router.get("/:id", customerControllers.getCustomer)
-router.post("/", [isAdmin, isAuth], customerControllers.addCutomer)
-router.put("/:id", [isAdmin, isAuth], customerControllers.updateCutomer)
-router.delete("/:id", [isAdmin, isAuth], customerControllers.deleteCutomer)
+router.post("/", customerControllers.addCutomer)
+router.put("/:id", customerControllers.updateCutomer)
+router.delete("/:id", customerControllers.deleteCutomer)
 
 
 module.exports = router;
